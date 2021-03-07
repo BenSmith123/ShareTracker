61ae1a31fc4a429cd6beedca394261832a501b72
BenSmith123 <benjiiman12@gmail.com>
2021-03-08 11:19:22 +1300

    remove unused

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ export default function App() {
 				Share overview
 			</Button>
 
-      <Button onClick={() => setDisplay(MENU_ITEMS.SHARE_SUMMARY)} >
+      <Button onClick={() => setDisplay(MENU_ITEMS.s)} >
 				Share summary
 			</Button>
 
@@ -63,58 +63,6 @@ export default function App() {
 
 	</>
 
-
-	
-
-    // <>
-    //     <Grid 
-    //       container 
-    //       item xs={12}
-    //       spacing={1} 
-    //       justify="space-around"
-    //       >
-
-    //     {companies.map((company) => {
-    //       return (
-    //           <>
-    //             <Grid className='card' xs={1}>
-    //               <img style={ {width: '50px', height: '50px'}}src={'https://data.sharesies.nz' + company.logos.thumb}></img>
-    //             </Grid>
-    //             <Grid className='card' xs={5}>
-    //               {company.name + ' (' + company.symbol + ')'}
-    //             </Grid>
-    //             <Grid className='card' xs={3}>
-    //               {company.marketPrice}
-    //             </Grid>
-    //             <Grid className='card' xs={3}>
-    //               {company.exchange}
-    //             </Grid>
-    //           </>
-    //         )}
-    //       )
-    //     }
-    //   </Grid>
-
- 
-    //   <form onSubmit={handleSubmit(onSubmit)}>
-    //     <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-    //     <input type="tel" placeholder="Mobile number" name="Mobile number" ref={register({required: true, minLength: 6, maxLength: 12})} />
-
-    //     <input name="Developer" type="radio" value="Yes" ref={register({ required: true })}/>
-    //     <input name="Developer" type="radio" value="No" ref={register({ required: true })}/>
-    //     <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
-    //     <select name="Title" ref={register({ required: true })}>
-    //       <option value="Mr">Mr</option>
-    //       <option value="Mrs">Mrs</option>
-    //       <option value="Miss">Miss</option>
-    //       <option value="Dr">Dr</option>
-    //     </select>
-
-    //     <input type="submit" />
-
-    // </form>
-
-    // </>
   );
 }
 
