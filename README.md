eab2f132734970107be0e723ce45f2a86f9700db
Ben Smith <bensmith@mediaworks.co.nz>
2021-03-12 09:56:21 +1300

    fix graph lables

---
+++ README.md
@@ -0,0 +1,9 @@
+
+### TODO
+- Find and deal with all TODO's in code
+- Function for getting all sharesies API data (either to live locally or be deployed to AWS lambda)
+- Determine scope - run project locally or host
+	- if not hosting this website, write local function/script to get sharesies data and store file locally to be used
+	- if hosted then move sharesies API interactions to lambda
+	- will also need to implement a login screen? Or input an API cookie to be used in lambda?
+- Crypto.com API socket implementation
--- src/shares/Summary.jsx
+++ src/shares/Summary.jsx
@@ -1,36 +1,45 @@
 
 import React from 'react';
-// import 'chartjs-plugin-datalabels';
+import 'chartjs-plugin-datalabels';
 
 import { Doughnut } from 'react-chartjs-2';
 
+// import { stats } from '../sharesies.private/stats.json';
 import { portfolio } from '../sharesies.private/check.json';
 import { instruments as companies } from '../sharesies.private/instruments-portfolio.json';
 
 
+// ordered arrays of processed data from sharesies API
 const investmentArrays = {
   names: [],
   contributions: [],
 };
 
+let totalContribution = 0;
 
-portfolio.sort((a, b) => (a.contribution - b.contribution));
 
-console.log(portfolio);
+portfolio.sort((a, b) => (b.contribution - a.contribution));
 
 portfolio.forEach((investment) => {
-  companies.find((company) => { // eslint-disable-line
+  companies.find((company) => { // eslint-disable-line array-callback-return
     if (company.id === investment.fund_id) {
+
       // avoid trying to figure out what obj the data comes from
       const data = { ...company, ...investment };
       investmentArrays.names.push(data.name);
+
+      // TODO - calcuelate currency diff - seperate arrays for NZD value and USD?
       investmentArrays.contributions.push(data.contribution);
+
+      totalContribution += parseInt(data.contribution);
     }
   });
 });
 
 
-console.log(investmentArrays);
+console.log(portfolio);
+
+console.log(totalContribution);
 
 
 // why this no work
@@ -42,65 +51,66 @@ console.log(investmentArrays);
 
 const chartOptions = {
   plugins: {
-    datalabels55: {
+    datalabels: {
+      // TODO - hide labels at the top and create new components for a list of companies
       display: true,
       color: 'white',
       anchor: 'end',
       font: {
-        weight: 'bold',
-        size: 20,
+        weight: 'none',
+        size: 12,
       },
-      formatter: (value) => {
-
-        console.log('hwllo');
-        console.log(value, value.toFixed(5));
-        return '';
-      }
-
-      // take the percentage (decimal), turn to integer by (x100) round to 1dp
-      // const percentage = (value.toFixed(3) * 100).toFixed(1);
-
-      // don't show percentage symbol if value is < 2 %
-      // return percentage < 3.5 ? percentage : `${percentage}%`;
-      ,
+      formatter: (value, { dataIndex }) => {
+        // calculate percentage and add a title
+        // TODO - add money value? - should be an option toggle (default off) for privacy
+        const percentage = ((parseFloat(value) / totalContribution) * 100).toFixed(1);
+        return `${percentage}%\n${investmentArrays.names[dataIndex]}`;
+      },
     },
   },
 };
 
+// http://tristen.ca/hcl-picker/#/hlc/13/1.05/DE80A7/E17E5B
+// #DE80A7,#C28BBD,#9C97CA,#6FA1CA,#42A9BE,#25ACA8,
+// #38AD8B,#5AAB6D,#7CA752,#9C9F40,#B9953C,#D18946,#E17E5B
 
 function renderPieChart(dataValues, dataLabels) {
   return (
-    <Doughnut data={{
-      labels: dataLabels,
-      options: chartOptions,
-      datasets: [{
-        label: '# of Votes',
-        data: dataValues,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      }],
-    }}
+    <Doughnut
+      options={chartOptions}
+      data={{
+        labels: dataLabels,
+        datasets: [{
+          label: '# of Votes',
+          data: dataValues,
+          backgroundColor: [ // TODO - globally define or automate chartjs colours
+            '#DE80A7',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        }],
+      }}
     />
   );
 }
 
 
 export default function Summary() {
+
+  // implement hooks for loading/updating data - once API is implemented
+
   return (
     <>
       {renderPieChart(investmentArrays.contributions, investmentArrays.names)}
